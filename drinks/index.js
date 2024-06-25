0788a1ee8d3f3ca63579f2121971b63a73fc56d0
Nekagda <375292229844@yandex.by>
2024-06-25 20:50:02 +0300

    1

--- drinks/index.js
+++ drinks/index.js
@@ -1,14 +1,16 @@
 import { ageVerificationPopup } from "../js/ageVerificationPopup.js";
 
-async function getData(category) {
+async function getData(category, classes) {
   fetch(`http://localhost:3000/menu?categoryId=${category}`)
     .then((response) => response.json())
     .then((data) => {
-      if (category === "Напитки") {
-        displayDrinks(data);
-      } else if (category === "Чай") {
-        displayDrinks1(data);
-      }
+      // if (category === "Напитки") {
+        displayDrinks(data, classes);
+      // } else if (category === "Чай") {
+      //   displayDrinks1(data);
+      // } else if (category === "Кофк") {
+      //   displayDrinks1(data);
+      // }
     })
     
     .catch((error) => console.error("Error fetching data:", error));
@@ -52,18 +54,17 @@ async function getData(category) {
 
 
 
-getData("Напитки")
-function displayDrinks(data) {
-  const cards = document.getElementsByClassName("napitki-head")[0];
+function displayDrinks(data, classes) {
+  const cards = document.getElementsByClassName(classes[0])[0];
   for (let i = 0; i < data.length; i++) {
     const rubels = Math.floor(data[i].price);
     const kopeiki = ((data[i].price - rubels) * 100).toFixed(0);
     const card = document.createElement("div");
-    card.classList.add("alk-content");
+    card.classList.add(classes[1]);
     card.innerHTML = `
-    <div class="napitki-name">
+    <div class="${classes[2]}">
               <p>${data[i].name}</p>
-              <div class="napitki-description">
+              <div class="${classes[3]}">
                 <p>${data[i].description}</p>
               </div>
             </div>
@@ -78,33 +79,10 @@ function displayDrinks(data) {
   }
 }
 
-getData("Чай");
-function displayDrinks1(data) {
-  const cards1 = document.getElementsByClassName("tea-block")[0];
-  cards1.innerHTML = '<div class="category"><img src="../img/tea-coffe2.png" alt="" />ЧАЙ</div>';
-  for (let i = 0; i < data.length; i++) {
-    const rubels1 = Math.floor(data[i].price);
-    const kopeiki1 = ((data[i].price - rubels1) * 100).toFixed(0);
-    
-    const card1 = document.createElement("div");
-    card1.classList.add("tea-content");
-    card1.innerHTML = `
-    <div class="tea-name">
-              <p>${data[i].name}</p>
-              <div class="tea-description">
-                <p>${data[i].description}</p>
-              </div>
-            </div>
-            <div class="tea-weight">
-              <p>${data[i].weight} мл.</p>
-            </div>
-            <div class="tea-price">
-              <p>${rubels1}Р<small>${kopeiki1}К</small></p>
-            </div>
-            `;
-    cards1.appendChild(card1);
-  }
-}
+getData("Напитки", ['napitki-head', 'alk-content', 'napitki-name', 'napitki-description']);
+getData("Чай", ['tea-block', 'alk-content', 'napitki-name', 'napitki-description']);
+getData("Кофе", ['coffe-block', 'alk-content', 'napitki-name', 'napitki-description']);
+
 
 
 // ageVerificationPopup();
